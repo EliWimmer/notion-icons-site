a6666eb2e8910ef7b461289552c002e7b5ad125e
EliWimmer <48037550+EliWimmer@users.noreply.github.com>
2021-10-30 15:34:52 -0600

    added radio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import iconParser from "./assets/components/iconParser";
 import SettingsToggle from "./assets/components/settingsToggle";
+import SettingsRadio from "./assets/components/settingsRadio";
 
 import link from "./assets/uiIcons/link.svg";
 import download from "./assets/uiIcons/download.svg";
@@ -7,22 +8,29 @@ import lightmode from "./assets/uiIcons/lightmode.svg";
 import darkmode from "./assets/uiIcons/darkmode.svg";
 import iconsimple from "./assets/uiIcons/iconsimple.svg";
 import iconfancy from "./assets/uiIcons/iconfancy.svg";
+import iconboth from "./assets/uiIcons/iconboth.svg";
 import labelshow from "./assets/uiIcons/labelshow.svg";
 import labelhide from "./assets/uiIcons/labelhide.svg";
+import settings from "./assets/uiIcons/settings.svg";
+import brain from "./assets/uiIcons/brain.svg";
 
 import { useState } from "react";
 import "./App.css";
 
 function App() {
 
+  // STATES
   const [darkMode, setDarkMode] = useState(false);
   const [downloadMode, setDownloadMode] = useState(false);
-  const [iconStyle, setIconStyle] = useState(false);
+  const [iconStyle, setIconStyle] = useState("both");
   const [labelMode, setLabelMode] = useState(false);
+  const [menuVisibility, setMenuVisibility] = useState(false);
 
+  // DATA SETS
   const icons = iconParser();
 
-  console.log(downloadMode);
+  // FUNCTIONS
+
   return (
     <>
       <div className={`App darkmode-${darkMode}`}>
@@ -32,7 +40,25 @@ function App() {
           <div className="main-menu">
 
           </div>
-          <div className={`settings-menu darkmode-${darkMode}`}>
+          <SettingsToggle
+            iconURL1={settings}
+            iconURL2={settings}
+            boolState={menuVisibility}
+            darkMode={darkMode}
+            label=""
+            stateOneText=""
+            stateTwoText=""
+            onClick={() => setMenuVisibility(!menuVisibility)}
+          />
+          <div className={`settings-menu darkmode-${darkMode} show-menu-${menuVisibility}`}>
+              <SettingsRadio
+              options={["simple", "fancy", "both", "brain"]}
+              icons={[iconsimple, iconfancy, iconboth, brain]}
+              activeOption={iconStyle}
+              onClick={(option) => setIconStyle(option)}
+              label="Icon Style"
+              darkMode={darkMode}
+              />
               <SettingsToggle 
               iconURL1={link} 
               iconURL2={download} 
@@ -54,17 +80,6 @@ function App() {
               onClick={() => setDarkMode(!darkMode)}
               label="Dark Mode"
               tooltipText="For visual purposes only. The downloaded icon will be the same."
-              />
-              <SettingsToggle
-              iconURL1={iconsimple}
-              iconURL2={iconfancy}
-              boolState={iconStyle}
-              darkMode={darkMode}
-              stateOneText="Simple"
-              stateTwoText="Fancy"
-              onClick={() => setIconStyle(!iconStyle)}
-              label="Icon Style"
-              tooltipText="Switched between simpler icons with a background, or fancier, bigger ones."
               />
               <SettingsToggle
               iconURL1={labelhide}
